148a07f470098851a66744d29aa25acf82321b56
Hassan Tawfik <h.khalaf16.75@gmail.com>
2024-01-07 01:46:10 +0200

    add to git

---
+++ GoogleCalendarEvents/GoogleCalendarEvents/IServices/IGoogleServices.cs
@@ -0,0 +1,17 @@
+﻿using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Calendar.v3;
+using GoogleCalendarEvents.DTOs;
+
+namespace GoogleCalendarEvents.IServices
+{
+    public interface IGoogleServices
+    {
+        Task<TokenResponse?> GetTokenAsync();
+        Task<CalendarService> GetCalendarServiceAsync();
+        Task<ResponseMessageDto?> AddCalendarEvent(EventRequestDto eventDto);
+        Task<List<EventResponseDto>> GetAllCalendarEventsAsync();
+        Task<EventResponseDto?> GetCalendarEventById(string id);
+        Task<ResponseMessageDto> DeleteCalendarEvent(string id);
+        Task<List<EventResponseDto>> SearchCalendarEvents(QueryParamsDto queryParams);
+    }
+}
